1a0edeada0802a1708d56a5e4914a1bd07d0f9fc
Ehud Eshet <ehud.eshet@imperva.com>
2019-04-29 17:00:13 +0300

    Add files via upload

---
+++ ThreadGroupSamples.java
@@ -0,0 +1,12 @@
+package com.incapsula.sampler;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class ThreadGroupSamples {
+
+    public HashMap<Long, String> threadNamesMap = new HashMap<>();
+    public TreeMap<String, SampleCount> samplingMap = new TreeMap<>();
+    public int maxDepth = 0;
+
+}
